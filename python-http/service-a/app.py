016db29ba34015755f06369d5995c222628d0de6
Ankush Makhijani <makhijaniankush24@gmail.com>
2026-02-04 15:01:54 -0800

    improved logging added comments and minor changes

--- python-http/service-a/app.py
+++ python-http/service-a/app.py
@@ -1,20 +1,40 @@
 from flask import Flask, request, jsonify
 import time
-import logging
+import structlog
+from utils import logging as logger
+import random
 
-logging.basicConfig(level=logging.INFO, format="%(asctime)s %(message)s")
 app = Flask(__name__)
+
+logger.configure_logging()
+log = structlog.get_logger()
+
+
+
 
 @app.get("/health")
 def health():
+    log.info("service = A endpoint = /health status = ok")
     return jsonify(status="ok")
 
 @app.get("/echo")
 def echo():
+    #=============================start the clock===================================
     start = time.time()
+    #=============================fetch the params==================================
     msg = request.args.get("msg", "")
+    
     resp = {"echo": msg}
-    logging.info(f'service=A endpoint=/echo status=ok latency_ms={int((time.time()-start)*1000)}')
+    #============================intentionally adding delay randomly======================
+    r = random.random()
+    if r < 0.4:
+        log.info("intentionally causing timeout")
+        time.sleep(2.0)   
+    elif r < 0.6:
+        log.info("intentionally causing causing delay")
+        time.sleep(0.7) 
+    #============================log responce==============================================
+    log.info(f'service=A endpoint=/echo status=ok latency_ms={int((time.time()-start)*1000)}')
     return jsonify(resp)
 
 if __name__ == "__main__":
--- python-http/service-b/app.py
+++ python-http/service-b/app.py
@@ -1,29 +1,38 @@
 from flask import Flask, request, jsonify
 import time
-import logging
 import requests
+import utils.logging as logger
+import structlog
 
-logging.basicConfig(level=logging.INFO, format="%(asctime)s %(message)s")
+logger.configure_logging()
+log = structlog.get_logger()
+
 app = Flask(__name__)
 
 SERVICE_A = "http://127.0.0.1:8080"
 
 @app.get("/health")
 def health():
+    log.info("service b, endpoint = /health status = ok")
     return jsonify(status="ok")
 
 @app.get("/call-echo")
 def call_echo():
+    """api end point call-echo, accepted param => ?msg, return json output"""
+    #-------------------------start clock--------------------------------------
     start = time.time()
+    #-------------------------fetch params-------------------------------------
     msg = request.args.get("msg", "")
     try:
-        r = requests.get(f"{SERVICE_A}/echo", params={"msg": msg}, timeout=1.0)
-        r.raise_for_status()
-        data = r.json()
-        logging.info(f'service=B endpoint=/call-echo status=ok latency_ms={int((time.time()-start)*1000)}')
+        r = requests.get(f"{SERVICE_A}/echo", params={"msg": msg}, timeout=(0.5, 1.0))
+        r.raise_for_status()    # check for HTTP error
+        data = r.json()         # convert to json format
+        #--------------------------------------log result------------------------------------------
+        log.info(f'service=B endpoint=/call-echo status=ok latency_ms={int((time.time()-start)*1000)}')
         return jsonify(service_b="ok", service_a=data)
     except Exception as e:
-        logging.info(f'service=B endpoint=/call-echo status=error error="{str(e)}" latency_ms={int((time.time()-start)*1000)}')
+        #------------------------------log exception--------------------------------------
+        log.exception(f'service=B endpoint=/call-echo status=error error="{str(e)}" latency_ms={int((time.time()-start)*1000)}')
         return jsonify(service_b="ok", service_a="unavailable", error=str(e)), 503
 
 if __name__ == "__main__":
